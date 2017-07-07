40d631a538b216fc62bdc7d5b6a724a86c6194c1
Stephen MacDonald <steviemac180@gmail.com>
2017-07-07 12:25:22 +0100

    initial commit

---
+++ pareto.R
@@ -0,0 +1,28 @@
+library(qcc)
+# pareto chart by Reason
+reason.data <- reason
+reason.names <- reason.data$Reason
+reason.counts <- reason.data$Total
+names(reason.counts) <- reason.names
+pareto.chart(reason.counts)
+
+# pareto chart by day
+day.data <- day
+day.names <- day.data$Reason
+day.counts <- day.data$Total
+names(day.counts) <- day.names
+pareto.chart(day.counts)
+
+# pareto chart by Reason
+reason.data <- Reason
+reason.names <- reason.data$Reason
+reason.counts <- reason.data$Total
+names(reason.counts) <- reason.names
+pareto.chart(reason.counts)
+
+# pareto chart by Reason
+reason.data <- Reason
+reason.names <- reason.data$Reason
+reason.counts <- reason.data$Total
+names(reason.counts) <- reason.names
+pareto.chart(reason.counts)
