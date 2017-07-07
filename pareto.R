dcee8e5337181b188fe4faca1d547bc3a9fbed16
Stephen MacDonald <steviemac180@gmail.com>
2017-07-07 12:35:11 +0100

    script complete

--- pareto.R
+++ pareto.R
@@ -16,13 +16,20 @@ pareto.chart(day.counts)
 # pareto chart by initials
 initials.data <- initials
 initials.names <- initials.data$Reason
-reason.counts <- reason.data$Total
-names(reason.counts) <- reason.names
-pareto.chart(reason.counts)
+initials.counts <- intials.data$Total
+names(intials.counts) <- initials.names
+pareto.chart(intials.counts)
 
-# pareto chart by Reason
-reason.data <- Reason
-reason.names <- reason.data$Reason
-reason.counts <- reason.data$Total
-names(reason.counts) <- reason.names
-pareto.chart(reason.counts)
+# pareto chart by location
+location.data <- location
+location.names <- location.data$location
+location.counts <- location.data$Total
+names(location.counts) <- location.names
+pareto.chart(location.counts)
+
+# pareto chart by source
+source.data <- source
+source.names <- source.data$Reason
+source.counts <- source.data$Total
+names(source.counts) <- source.names
+pareto.chart(source.counts)
